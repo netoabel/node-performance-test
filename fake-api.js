ab5c0485564b466263aa55bed21c5e7e2d4e6473
netoabel <abel.neto@gmail.com>
2021-09-05 11:18:11 -0300

    Changes time unit to always use miliseconds

--- fake-api.js
+++ fake-api.js
@@ -13,44 +13,43 @@ app.get('/slowest-endpoint', slowestEndpoint);
 function slowEndpoint(req, res){
     console.log('[/slow-endpoint] New incoming request.');
 
-    const minTimeInSeconds = 1;
-    const maxTimeInSeconds = 5;
+    const minTimeInMs = 10;
+    const maxTimeInMs = 3000;
 
-    respondAfterSomeTime(minTimeInSeconds, maxTimeInSeconds, res);
+    respondAfterSomeTime(minTimeInMs, maxTimeInMs, res);
 }
 
 function slowestEndpoint(req, res){
     console.log('[/slowest-endpoint] New incoming request.');
 
-    const minTimeInSeconds = 3;
-    const maxTimeInSeconds = 30;
+    const minTimeInMs = 3000;
+    const maxTimeInMs = 30000;
 
-    respondAfterSomeTime(minTimeInSeconds, maxTimeInSeconds, res);
+    respondAfterSomeTime(minTimeInMs, maxTimeInMs, res);
 }
 
-function respondAfterSomeTime(minTimeInSeconds, maxTimeInSeconds, res){
-    doAfterSomeTime(minTimeInSeconds, maxTimeInSeconds, (err, timeElapsedInMs) => {
+function respondAfterSomeTime(minTimeInMs, maxTimeInMs, res){
+    doAfterSomeTime(minTimeInMs, maxTimeInMs, (err, timeElapsedInMs) => {
         res.json({ message: `It took ${timeElapsedInMs} ms to load!` });
         
         console.log(`Response sent. (${timeElapsedInMs} ms)`);
     });
 }
 
-function doAfterSomeTime(minTimeInSeconds, maxTimeInSeconds, callback){
+function doAfterSomeTime(minTimeInMs, maxTimeInMs, callback){
     var startTimeInMs = Date.now();
 
-    waitSomeSeconds(minTimeInSeconds, maxTimeInSeconds, () => {
+    waitSomeSeconds(minTimeInMs, maxTimeInMs, () => {
         const timeElapsedInMs = Date.now() - startTimeInMs;
         callback(null, timeElapsedInMs);
     });
 }
 
 function waitSomeSeconds(min, max, callback) {
-    const seconds = getRandomIntegerInRange(min, max);
-    const miliseconds = seconds * 1000;
+    const timeInMs = getRandomIntegerInRange(min, max);
 
-    console.log(`Running some heavy stuff for ${seconds} seconds...`);
-    setTimeout(callback, miliseconds);
+    console.log(`Running some heavy stuff for ${timeInMs} ms...`);
+    setTimeout(callback, timeInMs);
 }
 
 function getRandomIntegerInRange(min, max) {
