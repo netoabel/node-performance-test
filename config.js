fbb6126b2ce14104f2361e05ec9b82d6f9072d7d
netoabel <abel.neto@gmail.com>
2021-09-06 16:44:34 -0300

    Adds .env file

---
+++ config.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+    serverPort: process.env.NODE_PORT,
+    apiUrl: process.env.API_URL,
+    slowRequestsCount: process.env.SLOW_REQUESTS_COUNT,
+    slowestRequestsCount: process.env.SLOWEST_REQUESTS_COUNT,
+    fakePayloadSizeInKB: process.env.FAKE_PAYLOAD_SIZE_KB,
+    latencies: {
+        slow: {
+            min: process.env.SLOW_ENDPOINT_MIN_SECONDS,
+            max: process.env.SLOW_ENDPOINT_MAX_SECONDS
+        },
+        slowest: {
+            min: process.env.SLOWEST_ENDPOINT_MIN_SECONDS,
+            max: process.env.SLOWEST_ENDPOINT_MAX_SECONDS
+        }
+    }
+};
+
+export default config;
